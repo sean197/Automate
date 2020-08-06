acc9e7b9071e4d148d08c4d5977466f0e6c963b0
learner <learner@justit.co.uk>
2020-08-06 17:36:58 +0100

    first commit

---
+++ Automaterteller/App_Start/RouteConfig.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace Automaterteller
+{
+    public class RouteConfig
+    {
+        // RegisterRoutes method is called when app starts
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            // IgnoreToute as any request with .axd
+            //.. will be for certain resources and system..
+            //... utitlities and dealt with by seperate handler
+            // *; treats last segment in url pattern as a catch all
+            // meaning any sequence 
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            routes.MapRoute(
+                name: "Serial number",
+                url: "serial/{letterCase}",
+                defaults: new { controller = "Home", action = "Serial", letterCase = "upper" }
+            );
+
+            // Call to map route
+            routes.MapRoute(
+                name: "Default",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+            );
+
+        }
+    }
+}
---
+++ Automaterteller/Controllers/HomeController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Automaterteller.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return PartialView();
+
+        }
+
+        // Post method activates when we send the form...
+        //... tag to the post
+        [HttpPost] 
+        // Contact method takes in a string parameter
+        public ActionResult Contact(string message)
+        {
+            // TODO: send message to HQ
+            ViewBag.Message = "Thanks BRAV.";
+
+            return View();
+        }
+
+        public ActionResult Foo()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            // The about page will open under Foo() method
+            return View("About");
+        }
+
+        public ActionResult Serial(string letterCase)
+        {
+            var serial = "ASPNETMVCSATM!";
+            if (letterCase == "lower")
+            {
+                return Content(serial.ToLower());
+            }
+            return Content(serial);
+        }
+    }
+}
